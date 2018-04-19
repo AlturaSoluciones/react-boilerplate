9823925662d5bc2ed699e0663a0c4a8cee35a7ab
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-19 17:31:21 -0500

    Linking Redux to React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+// generate redux logs
+import logger from 'redux-logger';
+// dispatch actions based on redux promise execution
+import ReduxPromise from 'redux-promise';
 import App from './App';
+import './index.css';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  applyMiddleware(ReduxPromise, logger)
+);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
