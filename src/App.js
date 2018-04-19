f43723a841ca563be26114a4a17ef39e6bec22a5
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-19 17:46:46 -0500

    Calling Redux to action

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+
+// calling the connect wrapper and the function created in the actions section
+import { connect } from 'react-redux';
+import { defaultFunction } from './actions';
 
 class App extends Component {
+  componentDidMount() {
+    // call default function to display redux operation
+    this.props.defaultFunction();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Altura Soluciones React Boilerplate
       </div>
     );
   }
 }
+  
+// function to convert the global state obtained from redux to local props
+const mapStateToProps = (state) => ({
+  default: state.default
+});
 
-export default App;
+
+export default connect(mapStateToProps, { defaultFunction })(App);
